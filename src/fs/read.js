10db3c4e0368bff709f5d3479c5a85020c01ad87
Daniil Khlybov <90640042+SteGlaset@users.noreply.github.com>
2023-06-20 12:24:10 +0300

    fix: update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -5,9 +5,10 @@ const FILE_DIR = 'files/';
 
 const read = async () => {
     try {
-        const filePath = path.join(FILE_DIR, 'fileToRemove.txt');
-
-        await fs.unlink(filePath)
+        const filePath = path.join(FILE_DIR, 'fileToRead.txt');
+        const data = await fs.readFile(filePath, { encoding: 'utf8' });
+        
+        console.log(data);
     } catch {
         console.log('FS operation failed')
     }
